7c68acfbd0f5974dcbf28887015199ff5c7444a5
Pedro Santos <1140386@isep.ipp.pt>
2016-06-02 15:38:37 +0100

    Adicionado o método get à classe produto

--- src/expo/model/Produto.java
+++ src/expo/model/Produto.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package expo.model;
 
 import java.util.ArrayList;
@@ -12,19 +11,22 @@ import java.util.List;
 
 public class Produto {
 
-	private String nome;
-	
-	public Produto()
-	{
-		nome = new String();
-	}
+    private String nome;
 
-	/**
-	 * 
-	 * @param nome
-	 */
-	public void setProduto(String nome) {
-		this.nome = nome;
-	}
+    public Produto(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     *
+     * @param nome
+     */
+    public void setProduto(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 
 }
