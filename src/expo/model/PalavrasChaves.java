34d9445166f6a5ad346ac582ac54261d8bd1dbb4
Pedro Santos <1140386@isep.ipp.pt>
2016-06-07 10:10:27 +0100

    Criada e implementada a classe PalavrasChaves

---
+++ src/expo/model/PalavrasChaves.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expo.model;
+
+/**
+ *
+ * @author Pedro Santos
+ */
+public class PalavrasChaves {
+      private String palavra;
+
+    public PalavrasChaves(String palavra) {
+        this.palavra = palavra;
+    }
+
+    /**
+     *
+     * @param palavra
+     */
+    public void setPalavrasChaves(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public String getPalavrasChaves() {
+        return palavra;
+    }
+
+}
+
+
